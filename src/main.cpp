2167ffb8a5a0fd34080a2d291989e40ab0c2de41
siva <sivaguru.t@zohocorp.com>
2020-07-26 22:04:43 +0530

    added menu bar

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <QKeySequence>
 #include <QMainWindow>
 #include <qtermwidget5/qtermwidget.h>
+#include <QMenuBar>
 
 int main(int argc, char *argv[])
 {
@@ -13,10 +14,26 @@ int main(int argc, char *argv[])
 
     QTermWidget *console = new QTermWidget();
 
-    mainWindow->resize(250, 150);
+    QMenuBar *menuBar = new QMenuBar(mainWindow);
+    QMenu *actionsMenu = new QMenu(QStringLiteral("Actions"), menuBar);
+    menuBar->addMenu(actionsMenu);
+    actionsMenu->addAction(QStringLiteral("Find..."), console, &QTermWidget::toggleShowSearchBar,
+                           QKeySequence(Qt::CTRL + Qt::SHIFT + Qt::Key_F));
+    actionsMenu->addAction(QStringLiteral("Copy"), console, &QTermWidget::copyClipboard,
+                           QKeySequence(Qt::CTRL + Qt::SHIFT + Qt::Key_C));
+    actionsMenu->addAction(QStringLiteral("Paste"), console, &QTermWidget::pasteClipboard,
+                           QKeySequence(Qt::CTRL + Qt::SHIFT + Qt::Key_V));
+    actionsMenu->addAction(QStringLiteral("About Qt"), &app, &QApplication::aboutQt);
+    mainWindow->setMenuBar(menuBar);
+
+    mainWindow->resize(600, 400);
     mainWindow->setWindowTitle("Segia");
 
     mainWindow->setCentralWidget(console);
+
+    QObject::connect(console, &QTermWidget::finished, mainWindow, &QMainWindow::close);
+
+    //mainWindow->setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
 
     mainWindow->show();
 
